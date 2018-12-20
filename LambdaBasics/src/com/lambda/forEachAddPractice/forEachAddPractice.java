6d95a875a88d61d6ba8c66015344e71531ff9a3d
Tanushree Ghosh <tanushree.ghosh12@gmail.com>
2018-12-20 17:10:44 +0530

    Changes

---
+++ LambdaBasics/src/com/lambda/forEachAddPractice/forEachAddPractice.java
@@ -0,0 +1,27 @@
+package com.lambda.forEachAddPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+    /**
+     * Created by 718895 on 12/20/2018.
+     */
+    public class forEachAddPractice {
+        public static void main(String... args){
+
+            List<String> strings = Arrays.asList("one", "two", "three", "four");
+            List<String> results = new ArrayList<String>();
+
+            Consumer<String> c1 = System.out::println;
+            Consumer<String> c2 = s -> results.add(s);
+
+            strings.forEach(c1);
+            strings.forEach(c1.andThen(c2));
+            System.out.println("Size of results: " + results.size());
+
+        }
+    }
+
+
